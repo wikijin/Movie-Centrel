2618a2a70326d65202f66237f92fb11ec6b8827f
Weiyu <alexwyjin@gmail.com>
2018-11-29 19:44:09 -0800

    Basic Set Up
    Basic Set Up

---
+++ src/test/java/cmpe275/dos/DosApplicationTests.java
@@ -0,0 +1,16 @@
+package cmpe275.dos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
