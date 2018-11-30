1c536ed98f8d5406d155df8d4a2f36cbc33f0c98
Weiyu <alexwyjin@gmail.com>
2018-11-29 19:57:59 -0800

    Add JsonConstant and UrlConstant Interface

---
+++ src/main/java/cmpe275/dos/constant/JsonConstant.java
@@ -0,0 +1,6 @@
+package cmpe275.dos.constant;
+
+public interface JsonConstant {
+    String KEY_ERROR = "error";
+    String KEY_MESSAGE = "message";
+}
---
+++ src/main/java/cmpe275/dos/constant/UrlConstant.java
@@ -0,0 +1,9 @@
+package cmpe275.dos.constant;
+
+public interface UrlConstant {
+    String USER = "/user";
+    String USERS = "/users";
+    String USER_ID = "/user/{userId}";
+    String LOGIN = "/login";
+    String HOST = "http://localhost";
+}
